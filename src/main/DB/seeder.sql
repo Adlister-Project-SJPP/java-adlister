a3c3e315a7a2f5122b20af979ec09a086659e0e2
Jonathan <jonathan.trhodes07@gmail.com>
2021-10-26 12:00:08 -0500

    databases complete

--- src/main/DB/seeder.sql
+++ src/main/DB/seeder.sql
@@ -1,3 +1,16 @@
 USE pet_lister;
 
+INSERT INTO users(username,email, password) VALUES
+        ('johnnyboy','johnnyB@gmail.com','letmein'),
+        ('amandaf','amandarocks@yahoo.com','letmein'),
+        ('sergiol','sergioboss@aol.com','letmein'),
+        ('patrickc','mastapatrick@yahoo.com','letmein');
+
+INSERT INTO ads(title, description, user_id,date) VALUES
+('Black labrador', 'He is the bestest boy ever', 2, 20201205);
+
+# INSERT INTO pet_type(dog, cat, reptile, bird, fish, ads_id) VALUES
+# ('L')
+
+
 
