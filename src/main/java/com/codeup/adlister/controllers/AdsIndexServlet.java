8e3c42e886b116e8e1620d52ebda2947ea0055cc
SerguioL <serguio.l.landa@gmail.com>
2021-10-26 21:57:51 -0500

    modified navbar to hide login when a user is a logged in and hide logut and profilr when no user is logged in

--- src/main/java/com/codeup/adlister/controllers/AdsIndexServlet.java
+++ src/main/java/com/codeup/adlister/controllers/AdsIndexServlet.java
@@ -13,6 +13,13 @@ import java.io.IOException;
 public class AdsIndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setAttribute("ads", DaoFactory.getAdsDao().all());
-        request.getRequestDispatcher("/WEB-INF/ads/index.jsp").forward(request, response);
+
+        if (request.getSession().getAttribute("user") != null) {
+            request.setAttribute("isLogin", true);
+            request.getRequestDispatcher("/WEB-INF/ads/index.jsp").forward(request, response);
+        }
+        response.sendRedirect("/login");
+//        request.getRequestDispatcher("/WEB-INF/ads/index.jsp").forward(request, response);
+
     }
 }
